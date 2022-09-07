b559a2f5f5dc50099aaa9e79a51536cd96f24f3a
honjow <honjow311@gmail.com>
2022-09-07 20:01:24 +0800

    add cf dialog

---
+++ lib/component/dialog/cf_dialog.dart
@@ -0,0 +1,101 @@
+import 'dart:io' as io;
+
+import 'package:eros_n/common/const/const.dart';
+import 'package:eros_n/common/global.dart';
+import 'package:eros_n/utils/logger.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:get/get.dart';
+
+// 使用请求nh主页，获取cookie更新到cookieJar
+Future<void> showInAppWebViewDialog({
+  int? statusCode,
+  VoidCallback? onComplete,
+}) async {
+  final CookieManager cookieManager = CookieManager.instance();
+  await cookieManager.deleteAllCookies();
+  final cookies = await showDialog<List<io.Cookie>>(
+      context: Get.context!,
+      barrierDismissible: false,
+      builder: (_) {
+        Widget iw() => InAppWebView(
+              initialUrlRequest: URLRequest(
+                url: Uri.parse(NHConst.baseUrl),
+              ),
+              onWebViewCreated: (controller) async {
+                final option = await controller.getOptions();
+                logger.d(
+                    'onWebViewCreated UA = ${option?.crossPlatform.userAgent}');
+                Global.userAgent = option?.crossPlatform.userAgent;
+                globalDioConfig =
+                    globalDioConfig.copyWith(userAgent: Global.userAgent);
+              },
+              initialOptions: inAppWebViewOptions,
+              shouldOverrideUrlLoading: (controller, navigationAction) async {
+                final uri = navigationAction.request.url!;
+
+                logger.d('to $uri');
+
+                return NavigationActionPolicy.ALLOW;
+              },
+              onLoadStop: (InAppWebViewController controller, Uri? uri) async {
+                logger.d('Page Finished loading: $uri');
+                if (uri == null) {
+                  return;
+                }
+
+                final cookies = await cookieManager.getCookies(url: uri);
+                logger.d(
+                    'cookies:\n${cookies.map((e) => e.toString()).join('\n')}');
+                if (cookies.length >= 2 &&
+                    cookies.any((element) => element.name == 'csrftoken')) {
+                  final ioCookies = cookies
+                      .map((e) => io.Cookie(e.name, '${e.value}'))
+                      .toList();
+
+                  Get.back<List<io.Cookie>>(result: ioCookies);
+                }
+              },
+            );
+
+        return AlertDialog(
+          // title: Text('获取数据中 $statusCode'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              // Text('获取数据中'),
+              const CircularProgressIndicator(),
+              SizedBox(
+                height: 0.1,
+                child: iw(),
+              ),
+            ],
+          ),
+        );
+      });
+
+  if (cookies != null) {
+    await Global.cookieJar
+        .saveFromResponse(Uri.parse(NHConst.baseUrl), cookies);
+    final rCookies =
+        await Global.cookieJar.loadForRequest(Uri.parse(NHConst.baseUrl));
+    logger.d('rCookies \n${rCookies.map((e) => e.toString()).join('\n')}');
+
+    onComplete?.call();
+  }
+
+}
+
+final InAppWebViewGroupOptions inAppWebViewOptions = InAppWebViewGroupOptions(
+  crossPlatform: InAppWebViewOptions(
+    useShouldOverrideUrlLoading: true,
+    mediaPlaybackRequiresUserGesture: false,
+    // userAgent: NHConst.userAgent,
+  ),
+  android: AndroidInAppWebViewOptions(
+    useHybridComposition: true,
+  ),
+  ios: IOSInAppWebViewOptions(
+    allowsInlineMediaPlayback: true,
+  ),
+);
--- lib/pages/nav/front/logic.dart
+++ lib/pages/nav/front/logic.dart
@@ -1,15 +1,11 @@
-import 'dart:io' as io;
-
 import 'package:eros_n/common/const/const.dart';
 import 'package:eros_n/common/global.dart';
+import 'package:eros_n/component/dialog/cf_dialog.dart';
 import 'package:eros_n/network/app_dio/pdio.dart';
 import 'package:eros_n/network/request.dart';
 import 'package:eros_n/utils/logger.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
-import 'package:webview_flutter/webview_flutter.dart' as wf;
 
 import 'state.dart';
 
@@ -44,7 +40,7 @@ class FrontLogic extends GetxController {
 
   Future<void> getGalleryData({
     bool refresh = false,
-    bool showWebviewDialogOnFail = true,
+    bool showWebViewDialogOnFail = true,
   }) async {
     final rCookies =
         await Global.cookieJar.loadForRequest(Uri.parse(NHConst.baseUrl));
@@ -58,10 +54,15 @@ class FrontLogic extends GetxController {
         state.galleryProviders.addAll(galleryList);
       }
     } on HttpException catch (e) {
-      if (showWebviewDialogOnFail && (e.code == 403 || e.code == 503)) {
+      if (showWebViewDialogOnFail && (e.code == 403 || e.code == 503)) {
         logger.e('code ${e.code}');
-        await showInAppWebViewDialog(statusCode: e.code);
-        await getGalleryData(refresh: refresh, showWebviewDialogOnFail: false);
+        await showInAppWebViewDialog(
+          statusCode: e.code,
+          onComplete: () async => await getGalleryData(
+            refresh: refresh,
+            showWebViewDialogOnFail: false,
+          ),
+        );
       } else {
         rethrow;
       }
@@ -72,114 +73,3 @@ class FrontLogic extends GetxController {
     await getGalleryData(refresh: true);
   }
 }
-
-Future<void> showInAppWebViewDialog({int? statusCode}) async {
-  final CookieManager cookieManager = CookieManager.instance();
-  await cookieManager.deleteAllCookies();
-  final cookies = await showDialog<List<io.Cookie>>(
-      context: Get.context!,
-      builder: (_) {
-        Widget iw() => InAppWebView(
-              initialUrlRequest: URLRequest(
-                url: Uri.parse(NHConst.baseUrl),
-              ),
-              onWebViewCreated: (controller) async {
-                final option = await controller.getOptions();
-                logger.d(
-                    'onWebViewCreated UA = ${option?.crossPlatform.userAgent}');
-                Global.userAgent = option?.crossPlatform.userAgent;
-                globalDioConfig =
-                    globalDioConfig.copyWith(userAgent: Global.userAgent);
-              },
-              initialOptions: inAppWebViewOptions,
-              shouldOverrideUrlLoading: (controller, navigationAction) async {
-                final uri = navigationAction.request.url!;
-
-                logger.d('to $uri');
-
-                return NavigationActionPolicy.ALLOW;
-              },
-              onLoadStop: (InAppWebViewController controller, Uri? uri) async {
-                logger.d('Page Finished loading: $uri');
-                if (uri == null) {
-                  return;
-                }
-
-                final cookies = await cookieManager.getCookies(url: uri);
-                logger.d(
-                    'cookies:\n${cookies.map((e) => e.toString()).join('\n')}');
-                if (cookies.length >= 2 &&
-                    cookies.any((element) => element.name == 'csrftoken')) {
-                  final ioCookies = cookies
-                      .map((e) => io.Cookie(e.name, '${e.value}'))
-                      .toList();
-
-                  Get.back<List<io.Cookie>>(result: ioCookies);
-                }
-              },
-            );
-
-        Widget wvf() => wf.WebView(
-              initialUrl: NHConst.baseUrl,
-              onPageFinished: (url) async {
-                logger.d('onPageFinished $url');
-                final cookies =
-                    await cookieManager.getCookies(url: Uri.parse(url));
-                logger.d(
-                    'cookies:\n${cookies.map((e) => e.toString()).join('\n')}');
-                if (cookies.any((element) => element.name == 'csrftoken')) {
-                  final ioCookies = cookies
-                      .map((e) => io.Cookie(e.name, '${e.value}'))
-                      .toList();
-                  Get.back<List<io.Cookie>>(result: ioCookies);
-                }
-              },
-              javascriptMode: wf.JavascriptMode.unrestricted,
-            );
-
-        return AlertDialog(
-          title: Text('获取数据中 $statusCode'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              // Text('获取数据中'),
-              Container(
-                height: 0.1,
-                child: iw(),
-              ),
-            ],
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Get.back();
-              },
-              child: Text('Hide'),
-            )
-          ],
-        );
-      });
-
-  if (cookies != null) {
-    await Global.cookieJar
-        .saveFromResponse(Uri.parse(NHConst.baseUrl), cookies);
-    final rCookies =
-        await Global.cookieJar.loadForRequest(Uri.parse(NHConst.baseUrl));
-    logger.d('rCookies \n${rCookies.map((e) => e.toString()).join('\n')}');
-  }
-  ;
-}
-
-final InAppWebViewGroupOptions inAppWebViewOptions = InAppWebViewGroupOptions(
-  crossPlatform: InAppWebViewOptions(
-    useShouldOverrideUrlLoading: true,
-    mediaPlaybackRequiresUserGesture: false,
-    // userAgent: NHConst.userAgent,
-  ),
-  android: AndroidInAppWebViewOptions(
-    useHybridComposition: true,
-  ),
-  ios: IOSInAppWebViewOptions(
-    allowsInlineMediaPlayback: true,
-  ),
-);
